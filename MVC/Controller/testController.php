d411942f2dcefc3119fb5e43c1e29c2909bbc322
erytan <146422767+erytan@users.noreply.github.com>
2023-12-23 23:38:46 +0700

    Update testController.php

--- MVC/Controller/testController.php
+++ MVC/Controller/testController.php
@@ -3,7 +3,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-//require ('../../vendor/phpmailer/src/Exception.php');
+include ('../../vendor/phpmailer/src/Exception.php');
 //require ('../../vendor/phpmailer/src/PHPMailer.php');
 //require ('../../vendor/phpmailer/src/SMTP.php');
 
