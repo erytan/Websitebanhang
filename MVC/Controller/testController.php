15e3a580eac81db8763c1b647f8869fd54605c70
erytan <146422767+erytan@users.noreply.github.com>
2023-12-23 23:43:16 +0700

    Update testController.php

--- MVC/Controller/testController.php
+++ MVC/Controller/testController.php
@@ -7,11 +7,11 @@ include ('../../vendor/phpmailer/src/Exception.php');
 include ('../../vendor/phpmailer/src/PHPMailer.php');
 include ('../../vendor/phpmailer/src/SMTP.php');
 
-include('../Model/ProductClass.php');
+//include('../Model/ProductClass.php');
 include('../Model/ProductService.php');
-include('../Model/BillClass.php');
+//include('../Model/BillClass.php');
 include('../Model/BillService.php');
-include('../Model/SploiClass.php');
+//include('../Model/SploiClass.php');
 include('../Model/SpLoiService.php');
 echo 'hehe'
 ?>
