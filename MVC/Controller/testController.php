0487fd151760063dce641371dbd7fdf2cf40b83c
erytan <ntan1695123@gmail.com>
2023-12-23 23:33:52 +0700

    .

---
+++ MVC/Controller/testController.php
@@ -0,0 +1,17 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require ('../../vendor/phpmailer/src/Exception.php');
+require ('../../vendor/phpmailer/src/PHPMailer.php');
+require ('../../vendor/phpmailer/src/SMTP.php');
+
+//require_once('../Model/ProductClass.php');
+require_once('../Model/ProductService.php');
+//require_once('../Model/BillClass.php');
+require_once('../Model/BillService.php');
+//require_once('../Model/SploiClass.php');
+require_once('../Model/SpLoiService.php');
+echo 'hehe'
+?>
