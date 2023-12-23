6ec43f4ca78ad4d50ec974d7ca2c224957074879
erytan <ntan1695123@gmail.com>
2023-12-23 23:29:59 +0700

    .

--- MVC/Controller/productController.php
+++ MVC/Controller/productController.php
@@ -7,11 +7,11 @@ require '../../vendor/phpmailer/src/Exception.php';
 require '../../vendor/phpmailer/src/PHPMailer.php';
 require '../../vendor/phpmailer/src/SMTP.php';
 
-require_once('../Model/ProductClass.php');
+//require_once('../Model/ProductClass.php');
 require_once('../Model/ProductService.php');
-require_once('../Model/BillClass.php');
+//require_once('../Model/BillClass.php');
 require_once('../Model/BillService.php');
-require_once('../Model/SploiClass.php');
+//require_once('../Model/SploiClass.php');
 require_once('../Model/SpLoiService.php');
 
 
