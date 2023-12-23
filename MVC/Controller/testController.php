2baf7a8429ae24da17f726b8871b0b5d4af154a9
erytan <146422767+erytan@users.noreply.github.com>
2023-12-23 23:35:45 +0700

    Update testController.php

--- MVC/Controller/testController.php
+++ MVC/Controller/testController.php
@@ -8,10 +8,10 @@ require ('../../vendor/phpmailer/src/PHPMailer.php');
 require ('../../vendor/phpmailer/src/SMTP.php');
 
 //require_once('../Model/ProductClass.php');
-require_once('../Model/ProductService.php');
+//require_once('../Model/ProductService.php');
 //require_once('../Model/BillClass.php');
-require_once('../Model/BillService.php');
+//require_once('../Model/BillService.php');
 //require_once('../Model/SploiClass.php');
-require_once('../Model/SpLoiService.php');
+//require_once('../Model/SpLoiService.php');
 echo 'hehe'
 ?>
