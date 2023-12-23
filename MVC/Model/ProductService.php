37f5b32851cde317c663dccd5f5401b2a4dcf51f
erytan <146422767+erytan@users.noreply.github.com>
2023-12-23 23:48:40 +0700

    Update ProductService.php

--- MVC/Model/ProductService.php
+++ MVC/Model/ProductService.php
@@ -1,7 +1,7 @@
 <?php
-require_once('ProductClass.php');
-require_once('ProductImage.php');
-require_once('../../connectdb.php');
+include ('ProductClass.php');
+include('ProductImage.php');
+include('../../connectdb.php');
 
 class ProductService{
     private $dbcollectProduct;
